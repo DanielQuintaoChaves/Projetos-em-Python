7571e6561bf4e2d629e9c83e473f59b4c380f694
DanielQC <danielquintaochaves@hotmail.com>
2024-10-18 02:52:17 -0300

    Update ex035.py

--- Exercícios/ex035.py
+++ Exercícios/ex035.py
@@ -1,6 +1,6 @@
 s1 = float(input('Digite o valor do segmento 1: '))
 s2 = float(input('Digite o valor do segmento 2: '))
-s3 = float(input('Digite o valor do segmento 3: '))
+s3 = float(input('Digite o valor do segmento 3: '))aaqq
 
 if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
     print('Os segmentos acima podem formar triângulo! ')
